3fe9b13720200a0cae862d3cfe6266a55e2d0048
ub-victor <victoireushindi371@gmail.com>
2025-09-18 20:18:43 +0200

    Initial commit

---
+++ Class/day1/simpleCalculator.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+// This program is a simple calculator that performs addition, subtraction, multiplication, and division.
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("Result: %.2lf\n", result);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Error: Invalid operator.\n");
+            break;
+    }
+
+    return 0;
+}
