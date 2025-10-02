75e54c361304fdc73c67361409338de4c75d2a3f
ub-victor <victoireushindi371@gmail.com>
2025-10-02 12:46:26 +0200

    c learning

---
+++ Season2/ArithmeticOperators.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	
+	int x = (2 + ((3 * 4) / 3)) - 2;
+	int y = 5 % 2;
+	printf("%i  %i\n ", x, y);
+
+	return 0;
+}
---
+++ Season2/Unary.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int money = 25;
+	int bill = 15;
+
+	int total = money -  - bill; // 25--15 = 25+15
+	int x = 5;
+    	int y = -x; // y will be -5
+
+	printf("%i %i\n", total, y);
+
+	return 0;
+}
