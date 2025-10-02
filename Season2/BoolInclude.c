ee947132266ffebb53a1a825c9722895c9399413
ub-victor <victoireushindi371@gmail.com>
2025-10-02 11:44:11 +0200

    c learning

---
+++ Season2/BoolInclude.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+// this allows us to use new key word bool at _bool and true at 1
+
+int main(){
+	bool calebIsFat = true;
+
+	printf("Is caleb fat %b", calebIsFat);
+
+	return 0;
+
+}
