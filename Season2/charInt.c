eebc1d15809a87bf7d2705bcb2f398d5ad866329
ub-victor <victoireushindi371@gmail.com>
2025-10-02 10:35:10 +0200

    c learning

--- Season2/charInt.c
+++ Season2/charInt.c
@@ -11,11 +11,12 @@ int main() {
     printf("Please enter an integer between 0 - 127: ");
     scanf("%i", &integer);  // Read the integer input
     getchar();  // Clear the newline after integer input
-    printf("%c\n", integer);  // Print the character corresponding to the ASCII value
+    printf("%c\n", integer);  // Print the character corresponding to the ASCII value and here we have to pay attention to the range of the integer some are newline or tab
+    // If the integer is out of range, the output may be unexpected or non-printable characters
 
     // Math with ASCII
     int mathz = 'A' + '\t';  // 'A' (65) + '\t' (9), store the sum as an integer
-    printf("A(65) + \\t(11) = %i\n", mathz );  // Print the result as the numeric sum
+    printf("A(65) + \\t(9) = %i\n", mathz );  // Print the result as the numeric sum
 
     return 0;
 }
