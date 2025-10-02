9dd8932488adf6582f3d42bdab3b10ac5678368e
ub-victor <victoireushindi371@gmail.com>
2025-10-02 11:06:09 +0200

    c learning

---
+++ Season2/Bool.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(){
+	_Bool vickyIsFat= 1; //boolean logic 1 = true 0= false 345 or any is  consider as 1 = true  
+	printf("Is Caleb Fat(1 is \"yes\" and 0 is \"no\")? %i\n", vickyIsFat);
+
+	return 0;
+
+
+}
