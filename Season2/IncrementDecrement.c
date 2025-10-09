ba0814f452975849b78c090c53b459a20c9a8309
ub-victor <victoireushindi371@gmail.com>
2025-10-09 20:25:16 +0200

    Libraries in C

--- Season2/IncrementDecrement.c
+++ Season2/IncrementDecrement.c
@@ -4,8 +4,10 @@
 int main (){
 	int pizzasToEat = 123;
 	pizzasToEat++;
+	int num = 10;
+	num += 10;
 
-	printf("Pizzas to eat are%d\n ", pizzasToEat);
+	printf("Pizzas to eat are %d\n and number is %d\n ", pizzasToEat, num);
 	
 	return 0;
 
---
+++ Season2/RandomInt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void){
+	for(int i = 0; i<5; ++i){
+		int r = rand();
+		printf("rand(): %d\n", r);
+                
+	}
+
+	return 0;
+}
