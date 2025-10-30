62a9cfcfbe7bb727732db45173b6661265c02480
ub-victor <victoireushindi371@gmail.com>
2025-10-30 10:02:55 +0200

    #include math

---
+++ Season2/IfStatements.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+
+	bool pizzaIsHealthy = true;
+	if(pizzaIsHealthy){// if(true){}
+
+		printf("Welcome to my pizza App\n");
+
+	}
+
+	return 0;
+}
