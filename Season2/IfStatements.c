15dd6ea493da37a5588813c0ec2a4e51ae9b8059
ub-victor <victoireushindi371@gmail.com>
2025-10-30 12:25:17 +0200

    Condition

--- Season2/IfStatements.c
+++ Season2/IfStatements.c
@@ -9,11 +9,15 @@ int main()
 	printf("Do you believe in the power of the pizza? 1 is true and 0 is false: ");
 	scanf("%d", &temp);
 	pizzaIsHealthy = temp;
+
 	if(pizzaIsHealthy){// if(true){}
 
 		printf("Welcome to my pizza App\n");
 
+	}else{
+		printf("Oh! You do not believe in Pizza's power!")
 	}
+	
 
 	return 0;
 }
