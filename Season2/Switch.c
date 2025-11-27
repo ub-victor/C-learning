d13ed03c40b2ca0de35e2ebf17e62c585cffdcca
ub-victor <victoireushindi371@gmail.com>
2025-11-27 20:55:53 +0200

    Complexe statement

--- Season2/Switch.c
+++ Season2/Switch.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 
-int main (){
-	int slice;
-	printf("Please choose a slice od pizza (1-4) \n");
-	scanf("%d", &slice);
+int main() {
+    int slice;
 
-	switch(slice)
-	{
-		case 1: 
-			printf("You chose the first \n ");
-			break;
-		case 2:
-			printf("You chose the second \n ");
-			break;
-		case 3: 
-			printf("You chose the third \n");
-			break;
-		case 4:
-			printf("You chose the forth \n");
-			break;
-		default:
-			printf("Bad choice you have to select 1-4\n ");
-			break;                                      		
-	}
-	return 0;
+    do {
+        printf("Please choose a slice of pizza (1-4), or press 0 to exit:\n");
+        scanf("%d", &slice);
+
+        switch (slice) {
+            case 1:
+                printf("You chose the first slice.\n");
+                break;
+            case 2:
+                printf("You chose the second slice.\n");
+                break;
+            case 3:
+                printf("You chose the third slice.\n");
+                break;
+            case 4:
+                printf("You chose the fourth slice.\n");
+                break;
+            case 0:
+                printf("Exiting program... Goodbye!\n");
+                break;
+            default:
+                printf("Bad choice! You must select 1–4 or 0 to exit.\n");
+                break;
+        }
+
+    } while (slice != 0);
+
+    return 0;
 }
+
