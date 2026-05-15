e5e8db9316668d4b281e388133ba5463c40008c3
ub-victor <victoireushindi371@gmail.com>
2026-05-15 04:02:48 +0200

    ml theory

--- Data-Structure/FinalDSA/reverce-array-in-place.c
+++ Data-Structure/FinalDSA/reverce-array-in-place.c
@@ -2,5 +2,35 @@
 Question 1: Reverse Array in place
 
 Explaination: 
+You are given an integer array and its length.you must reverse the order of elements without
+using extra array(in-place). For example, [1,2,3,4,5] becomes [5,4,3,2,1]. you can use a 
+temporary variable for swapping.
+
+Step-by-step Visualization
+
+[1, 2, 3, 4, 5]
+ ^
+             ^
+ left       right
+
+
 
 */
+
+void reverse_array(int* arr, int n){
+	// Initialize two  pointers: left at start, right at end
+	int left = 0;
+	int right = n-1;
+
+	//Continue swapping untill pointers meet or cross
+	while(left < right){
+		// Swap arr[left] and arr[right] using a temporary variable
+		int temp = arr[left]; // Store left value in temp
+		arr[left] = arr[right] //Copy right value to left position
+		arr[right] = temp;  // copy saved  letf value to right position
+
+		//move  pointers toward the certer
+		left++
+		right --;
+	}
+}
