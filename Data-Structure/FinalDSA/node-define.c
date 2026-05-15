27b92f654415452f539f32cdc07afccc02343fe6
ub-victor <victoireushindi371@gmail.com>
2026-05-15 10:43:11 +0200

    malloc

--- Data-Structure/FinalDSA/node-define.c
+++ Data-Structure/FinalDSA/node-define.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct Node {
-	int data;
-	struct Node* next;
+    int data;
+    struct Node* next;
 } Node;
+
+int main() {
+
+    Node n1;
+    Node n2;
+
+    n1.data = 10;
+    n1.next = &n2;
+
+    n2.data = 20;
+    n2.next = NULL;
+
+    printf("%d\n", n1.data);
+    printf("%d\n", n1.next->data);
+
+    return 0;
+}
